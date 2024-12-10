3d7c0960926157186f541e4d19913a9436192fc8
Sebastian <sebastian.kosciolek2000@gmail.com>
2024-12-10 19:22:40 +0100

    feat: add first step to business logic and first raport (sales)

---
+++ MVVMFirma/MVVMFirma/Models/BusinessLogic/DatabaseClass.cs
@@ -0,0 +1,22 @@
+﻿using MVVMFirma.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMFirma.Models.BusinessLogic
+{
+    public class DatabaseClass
+    {
+        #region Contex - DB
+        public InvoiceEntities db { get; set; }
+        #endregion
+        #region Constructor
+        public DatabaseClass(InvoiceEntities db)
+        {
+            this.db = db;
+        }
+        #endregion
+    }
+}
---
+++ MVVMFirma/MVVMFirma/Models/BusinessLogic/TowarB.cs
@@ -0,0 +1,33 @@
+﻿using MVVMFirma.Models.Entities;
+using MVVMFirma.Models.EntitiesForView;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMFirma.Models.BusinessLogic
+{
+    public class TowarB : DatabaseClass
+    {
+        #region Constructor
+        public TowarB(InvoiceEntities db)
+            : base(db) { }
+        #endregion
+        #region Business function
+        public IQueryable<KeyAndValue> GetTowaryKeyAndValueItems()
+        {
+            return 
+                (
+                    from towar in db.Towar
+                    select new KeyAndValue
+                    {
+                        Key=towar.IdTowaru,
+                        Value=towar.Name+" "+towar.Code
+                    }
+                ).ToList().AsQueryable();
+        }
+        #endregion
+    }
+}
