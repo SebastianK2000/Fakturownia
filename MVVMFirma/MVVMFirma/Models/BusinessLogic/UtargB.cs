75d18b05c9dc49f23af256c05d80c10dece7e0cf
Sebastian <sebastian.kosciolek2000@gmail.com>
2024-12-11 16:25:32 +0100

    feat: Add utargB file

---
+++ MVVMFirma/MVVMFirma/Models/BusinessLogic/UtargB.cs
@@ -0,0 +1,33 @@
+﻿using MVVMFirma.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMFirma.Models.BusinessLogic
+{
+    public class UtargB:DatabaseClass
+    {
+        #region Constructor
+        public UtargB(InvoiceEntities db)
+            :base(db) { }
+        #endregion
+        #region Business function
+        // funkcja do obliczania utargu danego towaru - okres od:do
+        public decimal? UtargOkresTowar(int idTowaru, DateTime dataOd, DateTime dataDo)
+        {
+            return
+                (
+                    from pozycja in db.InvoiceItems
+                    where
+                    pozycja.IdTowar == idTowaru && 
+                    pozycja.Invoice.Date>= dataOd && 
+                    pozycja.Invoice.Date<= dataDo
+                    select pozycja.Price*pozycja.Quantity
+                ).Sum();
+        }
+        #endregion
+    }
+}
